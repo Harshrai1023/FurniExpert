ba1d830fe987f72ca3e2dda9e33d397f80d4e3b9
Harshrai1023 <harshrai1023@gmail.com>
2024-05-09 14:27:31 +0530

    Login page added

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,118 @@
-import React from 'react'
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
+import { Button, Input, Typography } from "@material-tailwind/react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-export const LoginPage = () => {
+export function LoginPage() {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => setPasswordShown((cur) => !cur);
+
   return (
-    <div>LoginPage</div>
-  )
+    <section className="grid text-center h-screen justify-center items-center p-8">
+      <div className=" sm:w-[28rem] sm:px-10 p-5 border shadow-lg rounded-2xl">
+        <Typography variant="h3" color="blue-gray" className="mb-2">
+        Welcome Back
+        </Typography>
+        <Typography className="mb-6 text-gray-600 font-normal text-[18px]">
+          Sign in to continue
+        </Typography>
+        <form action="#" className="mx-auto max-w-[28rem] text-left">
+          <div className="mb-6">
+            <label htmlFor="email">
+              <Typography
+                variant="small"
+                className="mb-2 block font-medium text-gray-900"
+              >
+                Your Email
+              </Typography>
+            </label>
+            <Input
+              id="email"
+              color="gray"
+              size="lg"
+              type="email"
+              name="email"
+              placeholder="name@mail.com"
+              className="w-full placeholder:opacity-100 focus:border-t-primary border-t-blue-gray-200"
+              labelProps={{
+                className: "hidden",
+              }}
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password">
+              <Typography
+                variant="small"
+                className="mb-2 block font-medium text-gray-900"
+              >
+                Password
+              </Typography>
+            </label>
+            <Input
+              size="lg"
+              placeholder="********"
+              labelProps={{
+                className: "hidden",
+              }}
+              className="w-full placeholder:opacity-100 focus:border-t-primary border-t-blue-gray-200"
+              type={passwordShown ? "text" : "password"}
+              icon={
+                <i onClick={togglePasswordVisiblity}>
+                  {passwordShown ? (
+                    <EyeIcon className="h-5 w-5" />
+                  ) : (
+                    <EyeSlashIcon className="h-5 w-5" />
+                  )}
+                </i>
+              }
+            />
+          </div>
+          <Button color="gray" size="lg" className="mt-6" fullWidth>
+            sign in
+          </Button>
+          <div className="!mt-4 flex justify-end">
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              variant="small"
+              className="font-medium"
+            >
+              Forgot password
+            </Typography>
+          </div>
+          <div class="relative flex py-5 items-center">
+            <div class="flex-grow border-t border-gray-400"></div>
+            <span class="flex-shrink mx-4 text-gray-400">or</span>
+            <div class="flex-grow border-t border-gray-400"></div>
+          </div>
+          <Button
+            variant="outlined"
+            size="lg"
+            className="flex h-12 items-center justify-center gap-2"
+            fullWidth
+          >
+            <img
+              src={`https://www.material-tailwind.com/logos/logo-google.png`}
+              alt="google"
+              className="h-6 w-6"
+            />{" "}
+            sign in with google
+          </Button>
+          <Typography
+            variant="small"
+            color="gray"
+            className="!mt-4 text-center font-normal"
+          >
+            Not registered?{" "}
+            <NavLink to="/register" className="font-medium text-gray-900">
+              Create account
+            </NavLink>
+          </Typography>
+        </form>
+      </div>
+    </section>
+  );
 }
+
+export default LoginPage;
