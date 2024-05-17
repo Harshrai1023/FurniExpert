47cdfdb2d0e449ef42b86004849a56736ce729e4
Harshrai1023 <harshrai1023@gmail.com>
2024-05-17 17:01:50 +0530

    product guarantees and quantity button added with icon fixes

--- src/components/SingleProductDetails.jsx
+++ src/components/SingleProductDetails.jsx
@@ -1,13 +1,21 @@
+import {
+  CurrencyRupeeIcon,
+  HeartIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/outline";
 import { Button, Chip, Typography } from "@material-tailwind/react";
-import React from "react";
+import React, { useState } from "react";
+import { QuantityButtons } from "../widgets/QuantityButtons";
 export const SingleProductDetails = () => {
+  const [quantity,setQuantity] = useState(1);
+
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-4 p-5">
       <Chip className="w-min" value="New" />
-      <Typography>IN STOCK</Typography>
+      {/* <Typography>IN STOCK</Typography> */}
       <Typography>Foundations Matte Flip Flop</Typography>
       <Typography>(9.12k reviews)</Typography>
-      <Typography>$97.14</Typography>
+      <Typography>₹97.14</Typography>
       <Typography>
         Featuring the original ripple design inspired by Japanese bullet trains,
         the Nike Air Max 97 lets you push your style full-speed ahead.
@@ -15,35 +23,23 @@ export const SingleProductDetails = () => {
       <div className="h-full" />
       <div className="flex justify-between">
         <Typography>Quantity</Typography>
-        <div>
-          <div className="Add/Subtract Quantity h-5">asd</div>
-          <Typography>Quantity</Typography>
-        </div>
+        <QuantityButtons quantity={quantity} setQuantity={setQuantity}/>
       </div>
       <div className="border-t-2" />
       <div className="flex gap-2">
-        <Button fullWidth>Add to Cart</Button>
-        <Button fullWidth>Buy Now</Button>
+        <Button fullWidth className="flex items-center justify-center gap-2">
+          <ShoppingCartIcon className="h-5 w-5" /> Add to Cart
+        </Button>
+        <Button fullWidth className="flex items-center justify-center gap-2">
+          <CurrencyRupeeIcon className="h-5 w-5" /> Buy Now
+        </Button>
       </div>
       <div className="flex justify-center">
         <Button
           variant="text"
           className="w-min flex items-center gap-2 px-6 py-3"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            stroke="currentColor"
-            class="w-5 h-5"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-            ></path>
-          </svg>{" "}
+          <HeartIcon className="h-5 w-5" />
           Favorite
         </Button>
       </div>
--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,19 +1,49 @@
+import {
+  CheckBadgeIcon,
+  ClockIcon,
+  ShieldCheckIcon,
+} from "@heroicons/react/24/solid";
+import { Typography } from "@material-tailwind/react";
 import React from "react";
 import { SingleProductDetails } from "../components/SingleProductDetails";
 import { SingleProductImageGallery } from "../components/SingleProductImageGallery";
 export const SingleProductPage = () => {
   return (
-    <div className="flex flex-col lg:flex-row gap-5 max-w-7xl mx-auto p-5 ">
+    <div className="flex flex-col max-w-7xl mx-auto gap-5">
       {/* Product Image Gallery */}
-      <SingleProductImageGallery />
-      
-      {/* Product Details */}
-      <SingleProductDetails />
+      <div className="flex flex-col lg:flex-row gap-5 p-5">
+        <SingleProductImageGallery />
+        {/* Product Details */}
+        <SingleProductDetails />
+      </div>
 
       {/* Product guarantees */}
-      <div></div>
+      <div className="grid grid-cols-3 self-center gap-5 p-2 my-5">
+        <div className="flex flex-col justify-center items-center text-center gap-2 max-w-xs">
+          <CheckBadgeIcon className="h-8 w-8 fill-secondary" />
+          <Typography>100% Original</Typography>
+          <Typography>
+            Crafted from the finest materials for authentic and elegant pieces.
+          </Typography>
+        </div>
+        <div className="flex flex-col justify-center items-center text-center gap-2 max-w-xs">
+          <ClockIcon className="h-8 w-8 fill-secondary" />
+          <Typography>10 Day Replacement</Typography>
+          <Typography>
+            Easily replace any product within 10 days of purchase.
+          </Typography>
+        </div>
+        <div className="flex flex-col justify-center items-center text-center gap-2 max-w-xs">
+          <ShieldCheckIcon className="h-8 w-8 fill-secondary" />
+          <Typography>1 Year Warranty</Typography>
+          <Typography>
+            Enjoy a 1-year warranty on all our furniture products.
+          </Typography>
+        </div>
+      </div>
+      
       {/* Product Specifications & Reviews */}
-      <div></div>
+      {/* <div>Product Specifications & Reviews</div> */}
     </div>
   );
 };
