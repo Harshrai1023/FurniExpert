488017d7b746ca09b68d1b1d994f4138405551d7
Harshrai1023 <harshrai1023@gmail.com>
2024-05-18 18:47:47 +0530

    add product to cart added & minor fixes

--- src/components/SingleProductDetails.jsx
+++ src/components/SingleProductDetails.jsx
@@ -1,33 +1,65 @@
 import {
   CurrencyRupeeIcon,
   HeartIcon,
+  MinusIcon,
+  PlusIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/24/outline";
-import { Button, Chip, Typography } from "@material-tailwind/react";
+import { Button, Chip, IconButton, Typography } from "@material-tailwind/react";
 import React, { useState } from "react";
-import { QuantityButtons } from "../widgets/QuantityButtons";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../redux/cartRedux";
+
 export const SingleProductDetails = () => {
-  const [quantity,setQuantity] = useState(1);
+  const dispatch = useDispatch();
+  const [quantity, setQuantity] = useState(1);
+  const item = {
+    id: Date.now(),
+    img: "https://docs.material-tailwind.com/img/logos/logo-spotify.svg",
+    name: "Foundations Matte Flip Flop",
+    desc:"Featuring the original ripple design inspired by Japanese bullet trains, the Nike Air Max 97 lets you push your style full-speed ahead.",
+    price: 100,
+    quantity: quantity,
+  };
+  const handleIncrement = () => setQuantity(quantity + 1);
+  const handleDecrement = () =>
+    setQuantity(quantity - 1 > 0 ? quantity - 1 : 1);
 
   return (
     <div className="flex flex-col gap-4 p-5">
       <Chip className="w-min" value="New" />
+
       {/* <Typography>IN STOCK</Typography> */}
-      <Typography>Foundations Matte Flip Flop</Typography>
+      <Typography>{item.name}</Typography>
       <Typography>(9.12k reviews)</Typography>
-      <Typography>₹97.14</Typography>
+      <Typography>₹{item.price}</Typography>
       <Typography>
-        Featuring the original ripple design inspired by Japanese bullet trains,
-        the Nike Air Max 97 lets you push your style full-speed ahead.
+        {item.desc}
       </Typography>
       <div className="h-full" />
       <div className="flex justify-between">
         <Typography>Quantity</Typography>
-        <QuantityButtons quantity={quantity} setQuantity={setQuantity}/>
+        <div className="flex items-center border rounded-lg w-min p-1 gap-2">
+          <IconButton
+            className="rounded w-min h-min p-4"
+            variant="text"
+            onClick={handleDecrement}
+          >
+            <MinusIcon className="h-4 w-4" />
+          </IconButton>
+          <Typography>{quantity}</Typography>
+          <IconButton
+            className="rounded w-min h-min p-4"
+            variant="text"
+            onClick={handleIncrement}
+          >
+            <PlusIcon className="h-4 w-4" />
+          </IconButton>
+        </div>
       </div>
       <div className="border-t-2" />
       <div className="flex gap-2">
-        <Button fullWidth className="flex items-center justify-center gap-2">
+        <Button fullWidth className="flex items-center justify-center gap-2" onClick={() => dispatch(addProduct(item))}>
           <ShoppingCartIcon className="h-5 w-5" /> Add to Cart
         </Button>
         <Button fullWidth className="flex items-center justify-center gap-2">
